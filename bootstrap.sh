68e102b511b2722d5c8168b83f6dadc3c3c57bbe
Oleg <oleggavelya@gmail.com>
2026-03-17 11:52:30 +0200

    refactor: add chart name prefix to all resource names

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-helm install mysql ./helm-chart/todoapp/chart/mysql
 kubectl apply -f ./infrastructure/app/metrics-server.yml
 
-kubectl apply -f .infrastructure/app/pv.yml
+kubectl apply -f ./infrastructure/app/pv.yml
 helm install todoapp ./helm-chart/todoapp
 # Install Ingress Controller
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
