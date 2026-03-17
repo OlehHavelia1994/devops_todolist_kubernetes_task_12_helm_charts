42eb848e64313a58c12a4a75edddc2d86f58ffb6
Oleg <oleggavelya@gmail.com>
2026-03-17 13:38:40 +0200

    refactor: apply function range in deployments.env.sercets

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-kubectl apply -f ./infrastructure/app/metrics-server.yml
+kubectl apply -f .infrastructure/app/metrics-server.yml
 
-kubectl apply -f ./infrastructure/app/pv.yml
+kubectl apply -f .infrastructure/app/pv.yml
 helm install todoapp ./helm-chart/todoapp
 # Install Ingress Controller
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
